3d21b857764f0030d233aa7e2a4f5d5a7ba27075
a01032004 <a01032004@github.com>
2023-12-13 21:21:10 +0300

    Описываю элементы меню

--- main.cpp
+++ main.cpp
@@ -57,3 +57,101 @@ bool GamеStart(int &CountUsers, std::string &numberOfRound, int &typyOfCheckers
 //    us2.setPosition(680, 50);
 //    us2.setColor(Color::Blue);
 
+    sf::Text countUser1;
+    countUser1.setFont(font);
+    countUser1.setString(L"1");
+    countUser1.setCharacterSize(45);
+    countUser1.setFillColor(sf::Color::Black);
+    countUser1.setPosition(700, 87);
+
+    sf::Text countUser2;
+    countUser2.setFont(font);
+    countUser2.setString(L"2");
+    countUser2.setCharacterSize(45);
+    countUser2.setFillColor(sf::Color::Black);
+    countUser2.setPosition(800, 87);
+
+
+
+    //***********************************
+
+    sf::Text CountORounds;
+    CountORounds.setFont(font);
+    CountORounds.setString(L"Введите количество раундов:");
+    CountORounds.setCharacterSize(37);
+    CountORounds.setFillColor(sf::Color(237, 147, 0));
+    CountORounds.setOutlineThickness(3);
+    CountORounds.setOutlineColor(sf::Color::Black);
+    CountORounds.setPosition(100, 200);
+
+    sdx::TextBox::Text rounds("",534,320);
+    rounds.setSize(34);
+    sdx::TextBox enterCountRounds;
+    enterCountRounds.setPosition(700, 200);
+    enterCountRounds.setSize(100,40);
+    enterCountRounds.setBorder(1);
+
+
+    sf::Text orChoose;
+    orChoose.setFont(font);
+    orChoose.setString(L"или");
+    orChoose.setCharacterSize(37);
+    orChoose.setFillColor(sf::Color::Black);
+    orChoose.setPosition(850, 200);
+
+    sf::Text fastPlay;
+    fastPlay.setFont(font);
+    fastPlay.setString(L"быстрая игра");
+    fastPlay.setCharacterSize(37);
+    fastPlay.setFillColor(sf::Color::Black);
+    fastPlay.setPosition(980, 200);
+
+    //////************************************
+
+    sf::Text typeOfPlay;
+    typeOfPlay.setFont(font);
+    typeOfPlay.setString(L"Выберите вариант игры:");
+    typeOfPlay.setCharacterSize(37);
+    typeOfPlay.setFillColor(sf::Color(237, 147, 0));
+    typeOfPlay.setOutlineThickness(3);
+    typeOfPlay.setOutlineColor(sf::Color::Black);
+    typeOfPlay.setPosition(100, 315);
+
+    typyOfCheckers = 1;
+//    Texture types;
+//
+//    types.loadFromFile("images/type.png");
+//    Sprite t(types);
+//    t.setPosition(600, 300);
+
+    sf::Text type1;
+    type1.setFont(font);
+    type1.setString(L"русские");
+    type1.setCharacterSize(36);
+    type1.setFillColor(sf::Color::Black);
+    type1.setPosition(700, 315);
+
+    sf::Text type2;
+    type2.setFont(font);
+    type2.setString(L"поддавки");
+    type2.setCharacterSize(36);
+    type2.setFillColor(sf::Color::Black);
+    type2.setPosition(900, 315);
+
+    sf::Text type3;
+    type3.setFont(font);
+    type3.setString(L"английские");
+    type3.setCharacterSize(36);
+    type3.setFillColor(sf::Color::Black);
+    type3.setPosition(1150, 315);
+
+    sf::Text type4;
+    type4.setFont(font);
+    type4.setString(L"международные");
+    type4.setCharacterSize(36);
+    type4.setFillColor(sf::Color::Black);
+    type4.setPosition(1420, 315);
+
+
+    /////**********************color
+

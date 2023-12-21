6bd38cc70a59bfc5b9ce4b3f2a7a4f5e82951ead
TitoGobbi <153737371+TitoGobbi@users.noreply.github.com>
2023-12-21 18:21:58 +0300

    Закончил описывать английские
    Закончил описывать английские

--- 1111.cpp
+++ 1111.cpp
@@ -1102,3 +1102,266 @@ int playEnglishCheckers(int& CountUsers, std::string& numberOfRound, int& color,
                     wins2 = 0;
                     windowGame.close();
                     return 1123;
+
+
+                    if (eventGame.type == sf::Event::MouseButtonPressed) {
+
+                        if (eventGame.mouseButton.button == sf::Mouse::Left) {
+
+
+
+                            if (!repeatAttack)
+                            {
+                                for (int i = 0; i < BOARD_SIZE; i++) // в отдельную функцию.
+                                {
+                                    for (int j = 0; j < BOARD_SIZE; j++)
+                                    {
+                                        if ((i + j) % 2 == 0)
+                                            board[i][j].setFillColor(sf::Color::White);
+                                        else
+                                            board[i][j].setFillColor(sf::Color::Red);
+                                    }
+                                }
+                            }
+
+                            bool idk = false;
+                            int* goPos = new int[2] {};
+                            if (CountUsers == 1 && turn == idk)///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                            {
+                                std::vector<int*> attackers;
+                                for (int i = 0; i < 8; i++)
+                                    for (int j = 0; j < 8; j++)
+                                        if (board1[i][j] != nullptr && board1[i][j]->GetColor() == turn && ((board1[i][j]->GetAttackMoves(board1, 8, !turn).size() != 0) || (board1[i][j]->GetAttackMoves(board1, 8, !((board1[i][j]->isQueen() ^ turn))).size() != 0)))
+                                            attackers.push_back(new int[2] {i, j });
+                                std::vector<int*> movers;
+                                for (int i = 0; i < 8; i++)
+                                    for (int j = 0; j < 8; j++)
+                                        if (board1[i][j] != nullptr && board1[i][j]->GetColor() == turn && ((board1[i][j]->GetMoves(board1, 8, !turn).size() != 0) || (board1[i][j]->GetMoves(board1, 8, !((board1[i][j]->isQueen() ^ turn))).size() != 0)))
+                                            movers.push_back(new int[2] {i, j});
+                                if (int size = attackers.size() != 0)
+                                {
+                                    ///рандом на атакеров;
+                                    //выбор рандомного атакующего
+                                    if (!repeatAttack)
+                                    {
+                                        selectedPos = attackers[rand() % size];
+                                    }
+
+                                    if (board1[selectedPos[0]][selectedPos[1]]->isQueen()) {
+                                        // выбор рандомного хода данной шашки
+                                        attackMoves = VectorSum(board1[selectedPos[0]][selectedPos[1]]->GetAttackMoves(board1, 8, turn), board1[selectedPos[0]][selectedPos[1]]->GetAttackMoves(board1, 8, !turn));
+                                        int attackArrSize = attackMoves.size();
+                                        goPos = VectorSum(board1[selectedPos[0]][selectedPos[1]]->GetAttackMoves(board1, 8, turn), board1[selectedPos[0]][selectedPos[1]]->GetAttackMoves(board1, 8, !turn))[rand() % attackArrSize];
+                                    }
+                                    else {
+                                        // выбор рандомного хода данной шашки
+                                        attackMoves = board1[selectedPos[0]][selectedPos[1]]->GetAttackMoves(board1, 8, !turn);
+                                        int attackArrSize = attackMoves.size();
+                                        goPos = board1[selectedPos[0]][selectedPos[1]]->GetAttackMoves(board1, 8, !turn)[rand() % attackArrSize];/*Непонятка с тёрном*/
+                                    }
+
+                                }
+                                else if (int size = movers.size() != 0)
+                                {
+                                    selectedPos = movers[rand() % size];
+                                    moves = VectorSum(board1[selectedPos[0]][selectedPos[1]]->GetMoves(board1, 8, !turn), board1[selectedPos[0]][selectedPos[1]]->GetMoves(board1, 8, !(board1[selectedPos[0]][selectedPos[1]]->isQueen() ^ turn)));
+                                    int movesArrSize = moves.size();
+                                    goPos = moves[rand() % movesArrSize];
+                                    // аналогично
+
+                                }
+                                else {/// нет ходов гы.
+                                    return idk ? 2222 : 1111;
+                                }
+                            }
+
+                            sf::Vector2i mousePos = sf::Mouse::getPosition(windowGame);
+                            sf::Vector2i boardPos = (mousePos / TILE_SIZE);
+                            int* pos = new int[2] {((mousePos.y - 200) / TILE_SIZE), ((mousePos.x - 600) / TILE_SIZE) };
+                            // Проверка на наличие атаки
+                            bool hasAttack = false;
+                            for (int i = 0; i < BOARD_SIZE; i++)
+                                for (int j = 0; j < BOARD_SIZE; j++)
+                                    if (board1[i][j] != nullptr && board1[i][j]->GetColor() == turn && ((board1[i][j]->GetAttackMoves(board1, 8, !turn).size() != 0) || (board1[i][j]->GetAttackMoves(board1, 8, !(board1[i][j]->isQueen() ^ turn)).size() != 0)))
+                                        hasAttack = true;
+                            if (isPieceSelected && (inArray(moves, pos) || inArray(attackMoves, pos)) && pos[0] >= 0 && pos[1] >= 0 && pos[0] < 8 && pos[1] < 8 && board1[pos[0]][pos[1]] == nullptr || (CountUsers == 1 && turn == idk))
+                            {
+
+                                if (CountUsers == 1 && turn == idk)
+                                {
+                                    pos = goPos;
+                                }
+
+                                bool isAttack = inArray(attackMoves, pos); // передвижение / взятие
+                                board1[selectedPos[0]][selectedPos[1]]->moveTo(board1, pos[0], pos[1], isAttack, 8);
+                                hasAttack = false;
+                                if (turn && pos[0] == 0)
+                                {
+                                    board1[pos[0]][pos[1]]->SetQueen();
+                                }
+                                else if (!turn && pos[0] == 7)
+                                {
+                                    board1[pos[0]][pos[1]]->SetQueen();
+                                }
+
+                                if (isAttack && (attackMoves = VectorSum(board1[pos[0]][pos[1]]->GetAttackMoves(board1, 8, !turn), board1[pos[0]][pos[1]]->GetAttackMoves(board1, 8, board1[pos[0]][pos[1]]->isQueen() ? turn : !turn))).size() != 0)
+                                {
+                                    moves.clear();
+                                    for (int i = 0; i < BOARD_SIZE; i++) // в отдельную функцию.
+                                    {
+                                        for (int j = 0; j < BOARD_SIZE; j++)
+                                        {
+                                            if ((i + j) % 2 == 0)
+                                                board[i][j].setFillColor(sf::Color::White);
+                                            else
+                                                board[i][j].setFillColor(sf::Color::Red);
+                                        }
+                                    }
+                                    selectedPos[0] = pos[0]; selectedPos[1] = pos[1];
+
+                                    for (int i = 0; i < attackMoves.size(); ++i)
+                                        board[attackMoves[i][1]][attackMoves[i][0]].setFillColor(sf::Color::Yellow);
+                                    board[pos[1]][pos[0]].setFillColor(sf::Color::Green); // Добавить кнопку отмены хода.
+
+                                    repeatAttack = true;
+                                }
+                                else
+                                {
+                                    isPieceSelected = false; //конец хода.
+                                    turn = !turn;
+                                    repeatAttack = false;
+                                    for (int i = 0; i < BOARD_SIZE; i++) // в отдельную функцию.
+                                    {
+                                        for (int j = 0; j < BOARD_SIZE; j++)
+                                        {
+                                            if ((i + j) % 2 == 0)
+                                                board[i][j].setFillColor(sf::Color::White);
+                                            else
+                                                board[i][j].setFillColor(sf::Color::Red);
+                                        }
+                                    }
+                                }
+                            }
+
+                            else {
+                                if (board1[pos[0]][pos[1]] != nullptr && turn == board1[pos[0]][pos[1]]->GetColor() && !repeatAttack && (!hasAttack || ((board1[pos[0]][pos[1]]->GetAttackMoves(board1, 8, !turn).size() != 0) || (board1[pos[0]][pos[1]]->GetAttackMoves(board1, 8, !(board1[pos[0]][pos[1]]->isQueen() ^ turn)).size() != 0))))
+                                {
+
+                                    isPieceSelected = true;
+                                    selectedPos = new int[2] {pos[0], pos[1]};
+
+                                    if (!hasAttack)
+                                    {
+                                        moves = board1[pos[0]][pos[1]]->GetMoves(board1, 8, !turn);
+                                        if (board1[pos[0]][pos[1]]->isQueen())
+                                            moves = VectorSum(board1[pos[0]][pos[1]]->GetMoves(board1, 8, turn), moves);
+
+                                        for (int i = 0; i < moves.size(); ++i)
+                                            board[moves[i][1]][moves[i][0]].setFillColor(sf::Color::Blue);
+                                    }
+                                    else
+                                    {
+                                        attackMoves = board1[pos[0]][pos[1]]->GetAttackMoves(board1, 8, !turn);
+
+                                        if (board1[pos[0]][pos[1]]->isQueen())
+                                            attackMoves = VectorSum(board1[pos[0]][pos[1]]->GetAttackMoves(board1, 8, turn), attackMoves);
+                                        for (int i = 0; i < attackMoves.size(); ++i)
+                                            board[attackMoves[i][1]][attackMoves[i][0]].setFillColor(sf::Color::Yellow);
+                                    }
+                                    board[pos[1]][pos[0]].setFillColor(sf::Color::Green);
+
+
+                                }
+                            }
+                        }
+                    }
+                    //////////////////////////////////////////////////////////////////
+                    bool isWhiteWon = true;
+                    bool isBlackWon = true;
+                    for (int i = 0; i < BOARD_SIZE; ++i)
+                    {
+                        for (int j = 0; j < BOARD_SIZE; ++j)
+                        {
+                            if (board1[i][j] != nullptr)
+                            {
+                                if (board1[i][j]->GetColor() == true)
+                                    isBlackWon = false;
+                                else
+                                    isWhiteWon = false;
+                            }
+                        }
+                    }
+                    if (!isWhiteWon && !isBlackWon) {
+                        continue;
+                    }
+
+
+                    int temp = 0;
+
+                    if (isWhiteWon) {
+                        wins1 = wins1 + 1;
+                        numberOfRound = std::to_string(rounds - 1);
+                        if (std::stoi(numberOfRound) > 0) {
+                            return(11);
+                        }
+                        if (std::stoi(numberOfRound) == 0) {
+                            return (111);
+                        }
+                        return 1111;
+                    }
+
+                    if (isBlackWon) {
+                        wins2 = wins2 + 1;
+                        numberOfRound = std::to_string(rounds - 1);
+                        if (std::stoi(numberOfRound) == 0) {
+                            return (222);
+                        }
+                        if (std::stoi(numberOfRound) > 0) {
+                            return (22);
+                        }
+
+                        return 2222;
+                    }
+
+
+
+
+                    for (int i = 0; i < BOARD_SIZE; i++)
+                        for (int j = 0; j < BOARD_SIZE; j++)
+                            pieces[i][j].setFillColor(sf::Color::Transparent);
+
+                    for (int i = 0; i < BOARD_SIZE; i++) {
+                        for (int j = 0; j < BOARD_SIZE; j++) {
+                            if (board1[j][i] != nullptr && board1[j][i]->GetColor() == true) { // otdelynaya function pzlst
+                                pieces[i][j].setRadius(TILE_SIZE / 2 - 10);
+                                pieces[i][j].setFillColor(sf::Color::White);
+                                pieces[i][j].setPosition(i * TILE_SIZE + 610, j * TILE_SIZE + 210);
+                            }
+
+                            if (board1[j][i] != nullptr && board1[j][i]->GetColor() == false) {
+                                pieces[i][j].setRadius(TILE_SIZE / 2 - 10);
+                                pieces[i][j].setFillColor(sf::Color::Black);
+                                pieces[i][j].setPosition(i * TILE_SIZE + 610, j * TILE_SIZE + 210);
+                            }
+                        }
+                    }
+
+                    for (int i = 0; i < BOARD_SIZE; i++)
+                    {
+                        for (int j = 0; j < BOARD_SIZE; j++)
+                        {
+                            windowGame.draw(board[i][j]);
+
+                            if (pieces[i][j].getFillColor() != sf::Color::Transparent)
+                                windowGame.draw(pieces[i][j]);
+                        }
+                    }
+
+
+                    windowGame.display();
+
+    }
+
+    return 0;
+
+};
